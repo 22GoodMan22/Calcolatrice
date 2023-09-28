d1c6994b0105e41f7856b2359c6c8496f68c139b
Rocco <r.buongiorno2@studenti.unisa.it>
2023-09-28 16:08:20 +0200

    Funzioni somma3 e sottrazione 3 pk altre già esistenti

--- src/main/java/org/example/Calcolatrice.java
+++ src/main/java/org/example/Calcolatrice.java
@@ -15,18 +15,15 @@ public class Calcolatrice {
         return a+b;
     }
 
-    public int Somma3(int a, int b, int c)
-    {
+    public int Somma3(int a, int b, int c) {
         return a+b+c;
     }
 
     public int sottrazione(int a,int b){return a-b;}
 
-    public int Sottrazione3(int a, int b, int c)
-    {
+    public int sottrazione3(int a, int b, int c) {
         return a-b-c;
     }
-
     public int divisione(int a,int b){return a/b;}
 
     public int moltiplicazione(int a, int b){

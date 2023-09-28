6177684584fdeb4add4eef6c3074750a90b878f4
Rocco <r.buongiorno2@studenti.unisa.it>
2023-09-28 15:37:01 +0200

    Aggiunta funzione Somma3 e Sottrazione3 pk sottr e moltp già esistenti

--- src/main/java/org/example/Calcolatrice.java
+++ src/main/java/org/example/Calcolatrice.java
@@ -14,7 +14,19 @@ public class Calcolatrice {
     public int somma(int a, int b){
         return a+b;
     }
+
+    public int Somma3(int a, int b, int c)
+    {
+        return a+b+c;
+    }
+
     public int sottrazione(int a,int b){return a-b;}
+
+    public int Sottrazione3(int a, int b, int c)
+    {
+        return a-b-c;
+    }
+
     public int divisione(int a,int b){return a/b;}
 
     public int moltiplicazione(int a, int b){
